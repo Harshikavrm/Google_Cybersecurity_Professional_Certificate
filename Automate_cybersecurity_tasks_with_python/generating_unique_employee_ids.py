8312f83fb19c2b2c427cb29a4a926631ff28da37
Harshikavrm <139260466+Harshikavrm@users.noreply.github.com>
2024-04-01 21:12:55 +0530

    Create generating_unique_employee_ids.py

---
+++ Automate_cybersecurity_tasks_with_python/generating_unique_employee_ids.py
@@ -0,0 +1,10 @@
+# Assign the loop variable `i` to an initial value of 5000
+
+i = 5000
+
+# While loop that generates unique employee IDs for the Sales department by iterating through numbers
+# and displays each ID created
+
+while i<=5150: 
+    print(i)
+    i= i+1
